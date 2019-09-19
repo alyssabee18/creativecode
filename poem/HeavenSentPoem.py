193d86393aa139c3fcd4022fec73e7eefc7d9328
Alyssa <54549419+alyssabee18@users.noreply.github.com>
2019-09-18 21:00:37 -0400

    Update and rename frankenpoem.py to HeavenSentPoem.py

---
+++ poem/HeavenSentPoem.py
@@ -0,0 +1,11 @@
+from google.colab import files
+import markovify 
+
+uploaded = files.upload()
+for fn in uploaded.keys():
+  text = uploaded[fn].decode()
+
+text_model = markovify.Text(text)
+
+for i in range(6):
+  print(text_model.make_short_sentence(60))
